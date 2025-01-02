1c5548f72985dd0082291d6b4fbbb061ec9583be
Ifeanyi Dike <ifeanyi.dike@tryarbor.com>
2025-01-02 22:24:10 +0100

    set up grpc server to receive connections

--- ai-service/main.py
+++ ai-service/main.py
@@ -1,4 +1,14 @@
+import grpc
+from concurrent import futures
 
+
+def serve():
+    print("GRPC server listening...")
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+
+    server.add_insecure_port('[::]:50052')
+    server.start()
+    server.wait_for_termination()
 
 if __name__ == '__main__':
-    print("GRPC server listening...")
+    serve()
