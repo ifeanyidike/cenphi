d90c854dc9e970896efd572676f2517418d8c43a
Ifeanyi Dike <ifeanyi.dike@tryarbor.com>
2025-01-12 23:05:05 +0100

    create initial screen for the frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
@@ -6,6 +7,11 @@ export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
   server: {
     host: true,
