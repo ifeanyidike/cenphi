64682f571a791f770e3e9e82d1482ae16dffadb5
Ifeanyi Dike <ifeanyi.dike@tryarbor.com>
2025-03-14 17:41:10 +0100

    check back to original method after using alpine

--- api-server/internal/middleware/auth.go
+++ api-server/internal/middleware/auth.go
@@ -2,11 +2,8 @@ package middleware
 
 import (
 	"context"
-	"crypto/tls"
-	"crypto/x509"
 	"fmt"
 	"net/http"
-	"os"
 	"strings"
 
 	firebase "firebase.google.com/go/v4"
@@ -24,26 +21,6 @@ type AuthMiddleware struct {
 	logging *zap.Logger
 }
 
-// func NewAuthMiddleware(projectID string, logger *zap.Logger) (*AuthMiddleware, error) {
-// 	credentialsFile := "cenphiio-service-account.json"
-// 	path, err := config.FindFile(credentialsFile)
-
-// 	if err != nil {
-// 		return nil, fmt.Errorf("failed to find credentials file: %w", err)
-// 	}
-
-// 	opt := option.WithCredentialsFile(path)
-// 	app, err := firebase.NewApp(context.Background(), nil, opt)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	return &AuthMiddleware{
-// 		app:     app,
-// 		logging: logger,
-// 	}, nil
-// }
-
 func NewAuthMiddleware(projectID string, logger *zap.Logger) (*AuthMiddleware, error) {
 	credentialsFile := "cenphiio-service-account.json"
 	path, err := config.FindFile(credentialsFile)
@@ -52,39 +29,8 @@ func NewAuthMiddleware(projectID string, logger *zap.Logger) (*AuthMiddleware, e
 		return nil, fmt.Errorf("failed to find credentials file: %w", err)
 	}
 
-	// First try to use the system certificate pool
-	rootCAs, err := x509.SystemCertPool()
-	if rootCAs == nil {
-		rootCAs = x509.NewCertPool()
-	}
-
-	if err != nil {
-		fmt.Printf("error occurred in setting system ca: %v", err)
-	}
-
-	// If running in production, also try to load the specific CA file
-	if os.Getenv("ENV") == "production" {
-		caCert, err := os.ReadFile("/etc/pki/tls/certs/ca-bundle.crt")
-		if err == nil {
-			rootCAs.AppendCertsFromPEM(caCert)
-		} else {
-			// Log the error but continue with the system certs
-			logger.Warn("Failed to read CA file", zap.Error(err))
-		}
-	}
-
-	// Create a custom HTTP client with the certificate pool
-	httpClient := &http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{
-				RootCAs: rootCAs,
-			},
-		},
-	}
-
 	opt := option.WithCredentialsFile(path)
-	clientOpt := option.WithHTTPClient(httpClient)
-	app, err := firebase.NewApp(context.Background(), nil, opt, clientOpt)
+	app, err := firebase.NewApp(context.Background(), nil, opt)
 	if err != nil {
 		return nil, err
 	}
@@ -94,6 +40,57 @@ func NewAuthMiddleware(projectID string, logger *zap.Logger) (*AuthMiddleware, e
 		logging: logger,
 	}, nil
 }
+
+// func NewAuthMiddleware(projectID string, logger *zap.Logger) (*AuthMiddleware, error) {
+// 	credentialsFile := "cenphiio-service-account.json"
+// 	path, err := config.FindFile(credentialsFile)
+
+// 	if err != nil {
+// 		return nil, fmt.Errorf("failed to find credentials file: %w", err)
+// 	}
+
+// 	// First try to use the system certificate pool
+// 	rootCAs, err := x509.SystemCertPool()
+// 	if rootCAs == nil {
+// 		rootCAs = x509.NewCertPool()
+// 	}
+
+// 	if err != nil {
+// 		fmt.Printf("error occurred in setting system ca: %v", err)
+// 	}
+
+// 	// If running in production, also try to load the specific CA file
+// 	if os.Getenv("ENV") == "production" {
+// 		caCert, err := os.ReadFile("/etc/pki/tls/certs/ca-bundle.crt")
+// 		if err == nil {
+// 			rootCAs.AppendCertsFromPEM(caCert)
+// 		} else {
+// 			// Log the error but continue with the system certs
+// 			logger.Warn("Failed to read CA file", zap.Error(err))
+// 		}
+// 	}
+
+// 	// Create a custom HTTP client with the certificate pool
+// 	httpClient := &http.Client{
+// 		Transport: &http.Transport{
+// 			TLSClientConfig: &tls.Config{
+// 				RootCAs: rootCAs,
+// 			},
+// 		},
+// 	}
+
+// 	opt := option.WithCredentialsFile(path)
+// 	clientOpt := option.WithHTTPClient(httpClient)
+// 	app, err := firebase.NewApp(context.Background(), nil, opt, clientOpt)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	return &AuthMiddleware{
+// 		app:     app,
+// 		logging: logger,
+// 	}, nil
+// }
 
 func (m *AuthMiddleware) VerifyToken(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
