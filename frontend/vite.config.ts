e68750a15ecc5ab6455d5eb30f4a03763ca5fca1
Ifeanyi Dike <ifeanyi.dike@tryarbor.com>
2025-04-18 11:41:05 +0100

    try simple deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,32 @@ import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
+//   },
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+//   server: {
+//     host: true,
+//     port: 3000, // This is the port which we will use in docker
+//     // Thanks @sergiomoura for the window fix
+//     // add the next lines if you're using windows and hot reload doesn't work
+//     watch: {
+//       usePolling: true,
+//     },
+//     headers: {
+//       "Cross-Origin-Opener-Policy": "same-origin",
+//       "Cross-Origin-Embedder-Policy": "require-corp",
+//     },
+//   },
+// });
+
 export default defineConfig(({ mode }) => {
   const isProduction = mode === "production";
 
@@ -15,16 +41,9 @@ export default defineConfig(({ mode }) => {
         "@": path.resolve(__dirname, "./src"),
       },
     },
-    build: {
-      outDir: "dist",
-      sourcemap: true,
-      rollupOptions: {
-        external: ["react", "react-dom"],
-      },
-    },
     server: {
       host: true,
-      port: 3000,
+      port: 3000, // This is the port which we will use in Docker
       watch: {
         usePolling: true,
       },
