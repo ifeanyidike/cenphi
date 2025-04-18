1c63285504906f9d181ba05eb747dc8c5e8ba4c1
Ifeanyi Dike <ifeanyi.dike@tryarbor.com>
2025-04-18 17:05:46 +0100

    deploy widget to github pages

--- frontend/build-widget.js
+++ frontend/build-widget.js
@@ -1,7 +1,12 @@
 // build-widget.js
-const { execSync } = require("child_process");
-const fs = require("fs");
-const path = require("path");
+import { execSync } from "child_process";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Get __dirname equivalent in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Create widget build directory if it doesn't exist
 const widgetDir = path.join(__dirname, "public", "widget");
